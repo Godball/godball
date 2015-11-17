300d9435f29293626e747da3682a39bfd85b4e9a
Staffan Sandberg <Staffan.Sandberg94@gmail.com>
2015-11-17 16:06:48 +0100

    Initial commit

---
+++ Assets/Scripts/TouchTest.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchTest : MonoBehaviour {
+
+	public float speed = 100f;
+	private Vector2 startPosition;
+	private Vector2 endPosition;
+	private Vector2 direction;
+	private Rigidbody rb;
+
+	void Start()
+	{
+		rb = gameObject.GetComponent<Rigidbody> ();
+	}
+
+	void Update() {
+		if (Input.GetMouseButtonDown (0))
+		{
+			startPosition = Input.mousePosition; // Get first mouse position
+		}
+		if (Input.GetMouseButtonUp (0)) 
+		{
+			endPosition = Input.mousePosition;	// Get second mouse position
+			direction = endPosition - startPosition; // Get direction
+			direction.Normalize();	// Normalize the direction
+			//Debug.Log(direction.ToString());
+
+			// Add force to gameObject
+			rb.AddForce(new Vector3(direction.x,0,direction.y)*speed,ForceMode.VelocityChange);
+
+
+		}
+	}
+}
