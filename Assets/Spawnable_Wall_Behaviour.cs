f43e14152a084fc8a6ec431f60b67c8ca3257680
Douglas Carlsson <douglas.carlsson@gmail.com>
2015-11-26 10:20:56 +0100

    Added a spwanable wall

---
+++ Assets/Spawnable_Wall_Behaviour.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawnable_Wall_Behaviour : MonoBehaviour {
+
+    public float speed = 1.0F;
+    public int lifespan = 3;
+
+    private Vector3 startMarker;
+    private Vector3 endMarker;
+    private float startTime;
+    private float journeyLength;
+    void Start()
+    {
+        startTime = Time.time;
+        startMarker = new Vector3(
+            transform.position.x,
+            transform.position.y,
+            transform.position.z);
+        endMarker = startMarker + new Vector3(0, 1, 0);
+        journeyLength = Vector3.Distance(startMarker, endMarker);
+    }
+    void Update()
+    {
+        if ((Time.time - startTime) > lifespan)
+            Destroy(gameObject);
+
+
+        float distCovered = (Time.time - startTime) * speed;
+        float fracJourney = distCovered / journeyLength;
+        Debug.Log(string.Format("Start: {0} - End: {1} - Frac: {2}", startMarker.ToString(), endMarker.ToString(), fracJourney.ToString()));
+        transform.position = Vector3.Lerp(startMarker, endMarker, fracJourney);
+    }
+}
---
+++ Assets/TestPlayer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestPlayer : MonoBehaviour {
+
+    public Object prefab;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            // Spawn prefab
+            Object wall = Instantiate(prefab, new Vector3(5.0f, -0.6f, 0.0f), Quaternion.identity);
+        }
+    }
+}
