33cbd32f5853398e900f04961ecc0d65b3ca10a0
Staffan Sandberg <Staffan.Sandberg94@gmail.com>
2015-11-29 13:11:43 +0100

    Added a Scene which acts as a menu. The menu will load the game when a hero is selected.

---
+++ Assets/Scripts/MenuHandeler.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+/* Functions that do stuff when a button is pressed
+ * buttons can have functions attached to them in the inspector.
+ */
+public class MenuHandeler : MonoBehaviour {
+
+	public Canvas mainMenu;
+	public Canvas heroSelection;
+	public Canvas options;
+
+	void Awake()
+	{
+		mainMenu.enabled = true;
+		heroSelection.enabled = false;
+		options.enabled = false;
+	}
+
+	// Main menu Functions for Buttons
+	public void PlayButton()
+	{
+		mainMenu.enabled = false;
+		heroSelection.enabled = true;
+	}
+
+	public void OptionsButton()
+	{
+		options.enabled = true;
+		mainMenu.enabled = false;
+	}
+
+	public void QuitButton()
+	{
+
+	}
+
+	//Hero Selection Functions for Buttons
+	public void SlectThor()
+	{
+		Application.LoadLevel ("TestLevel");
+	}
+
+	public void SelectZeus()
+	{
+		Application.LoadLevel ("TestLevel");
+	}
+
+
+	// Function used in both Options and Hero Selection
+	public void BackToMainMenu()
+	{
+		mainMenu.enabled = true;
+		heroSelection.enabled = false;
+		options.enabled = false;
+	}
+}
