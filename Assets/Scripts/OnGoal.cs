4504526f0d3463e784101ec9107aeebade826bc0
Denis Kulicek <denis.kulicek@gmail.com>
2015-12-10 16:58:50 +0100

    Sounds added (partially)

--- Assets/Scripts/OnGoal.cs
+++ Assets/Scripts/OnGoal.cs
@@ -5,11 +5,15 @@ public class OnGoal : MonoBehaviour {
 
 	private Rigidbody rb;
 	public ScoreCount scoreCount; // Reference to other Script
+	public AudioClip score;
+	public AudioSource audio;
 
 	void Start()
 	{
 		scoreCount = gameObject.GetComponent<ScoreCount> ();
 		rb = GetComponent<Rigidbody> ();
+		audio = GetComponent<AudioSource>();
+		score = (AudioClip) Resources.Load("Sounds/score.wav");
 	}
 
 	void OnTriggerEnter(Collider collision)
@@ -18,6 +22,8 @@ public class OnGoal : MonoBehaviour {
 		rb.velocity = Vector3.zero;
 		rb.angularVelocity = Vector3.zero;
 		transform.position = new Vector3(0,3,0);
+
+		audio.PlayOneShot (score, 1.0F);
 
 		if (collision.gameObject.tag == "Player1Goal") 
 		{
